700e2f5b9e61079f1855dd90cb57968c2192959f
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-02-19 01:07:06 +0530

    feat: add cookie-parser middleware for handling cookies in requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //global middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", routes);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,18 @@ router.get("/", (req, res) => {
   res.json({ message: "api running" });
 });
 
+//spotify auth route
+router.get("/auth/spotify/callback", async (req, res) => {
+  const { code, error } = req.query;
+  if (error) {
+    return res.status(400).json({ error });
+  }
+  if (!code) {
+    return res.status(400).json({ message: "No code recieved" });
+  }
+  res.json({ message: "Spotify callback recieved", code });
+});
+
 export default router;
 
 //checking prisma working
